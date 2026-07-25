08a20d2881ecdcd19863345340889c6fea6f8ba4
Kavibalan <kavibalan1904@gmail.com>
2026-07-26 00:09:48 +0530

    Add root index.js entry point for Wispbyte and Pterodactyl panels

---
+++ index.js
@@ -0,0 +1,5 @@
+/**
+ * Root Entry Point for Wispbyte & Game Panels (Pterodactyl / Wisp)
+ * This file allows you to set either "index.js" OR "src/index.js" as the Main File on Wispbyte.
+ */
+require('./src/index.js');
